df94da4c1a6f0d2786372784608b1b1f647e6195
MarioM <mmontoj@gmail.com>
2023-12-05 16:13:42 +0100

    Primer codigo en CMake

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,12 @@
+﻿// hello_world.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "hello_world.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
